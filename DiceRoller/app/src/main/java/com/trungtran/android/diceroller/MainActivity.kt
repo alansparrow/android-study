52f3d0c5eaa98922fe5c5cf81756356b196983d6
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2022-07-23 23:33:16 +0100

    finishes Dice Roller app

--- DiceRoller/app/src/main/java/com/trungtran/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/trungtran/android/diceroller/MainActivity.kt
@@ -3,15 +3,17 @@ package com.trungtran.android.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.trungtran.android.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,5 +38,29 @@ fun DiceRollApp() {
 @Preview(showBackground = true)
 @Composable
 fun DiceWithButtonImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
 
+    val imgRes = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        6 -> R.drawable.dice_6
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imgRes),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(id = R.string.roll))
+        }
+    }
 }
